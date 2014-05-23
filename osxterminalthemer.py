b13cd08f667f7d1886f55ddeab0014689a19e844
Seena Burns <hello@seenaburns.com>
2014-05-22 22:48:00 -0700

    Unpackage bplist data, convert to xml, encode as json in unpackage_theme

--- osxterminalthemer.py
+++ osxterminalthemer.py
@@ -1,11 +1,56 @@
 import sys
 import subprocess
 import plistlib
+import json
+
+bplist_keys = ['ANSIBlackColor',
+               'ANSIBlueColor',
+               'ANSICyanColor',
+               'ANSIGreenColor',
+               'ANSIMagentaColor',
+               'ANSIRedColor',
+               'ANSIWhiteColor',
+               'ANSIYellowColor',
+               'ANSIBrightBlackColor',
+               'ANSIBrightBlueColor',
+               'ANSIBrightCyanColor',
+               'ANSIBrightGreenColor',
+               'ANSIBrightMagentaColor',
+               'ANSIBrightRedColor',
+               'ANSIBrightWhiteColor',
+               'ANSIBrightYellowColor',
+               'BackgroundColor',
+               'SelectionColor',
+               'TextColor',
+               'TextBoldColor',
+               'CursorColor',
+               'Font']
 
 def unpackage_theme(pl_string):
     theme_pl = plistlib.readPlistFromString(pl_string)
     
-    print theme_pl
+    # Extract plist for each encoded key/value
+    for k,v in theme_pl.iteritems():
+        if k not in bplist_keys:
+            continue
+
+        bplist = bplist_to_xml(v.data)
+        theme_pl[k] = plistlib.readPlistFromString(bplist)
+
+    print json.dumps(theme_pl, default=lambda o: o.__dict__, indent=4)
+    
+def bplist_to_xml(data):
+    cmd = "plutil -convert xml1 - -o -"
+    p = subprocess.Popen(cmd, shell=True,
+                         stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE)
+    p.stdin.write(data)
+    stdout, stderr = p.communicate()
+
+    return stdout
+
+def xml_to_bplist(data):
+    pass
 
 if __name__ == '__main__':
     f = open(sys.argv[1], 'r')
